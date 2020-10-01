d49e630a314422266e9ef50f87249a841538f77f
albertgc <gualbertpcuizav@gmail.com>
2020-10-01 13:43:14 -0400

    home page disegned

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavComponent } from './shared/nav/nav.component';
+import { ProjectComponent } from './home/components/project/project.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavComponent
+    NavComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
