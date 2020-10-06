4e31f5a1755767e744ff941df361219781a4c321
albertgc <gualbertpcuizav@gmail.com>
2020-10-06 12:16:16 -0400

    firebase rules add

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAGTBhSj53V5AcEOm1BNjhV6aK9gTh7vK8",
+    authDomain: "albertgc-cba65.firebaseapp.com",
+    databaseURL: "https://albertgc-cba65.firebaseio.com",
+    projectId: "albertgc-cba65",
+    storageBucket: "albertgc-cba65.appspot.com",
+    messagingSenderId: "581102392852",
+    appId: "1:581102392852:web:0648c3a2c28db8df52f901",
+    measurementId: "G-1SJQ850CM7"
+  }
 };
