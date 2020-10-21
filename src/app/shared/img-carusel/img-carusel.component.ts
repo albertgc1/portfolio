a05415165e2fb33628cfa5550e40ae5b7017e522
albertgc <gualbertpcuizav@gmail.com>
2020-10-21 19:49:19 -0400

    change arrow icon color

--- src/app/shared/img-carusel/img-carusel.component.ts
+++ src/app/shared/img-carusel/img-carusel.component.ts
@@ -2,7 +2,8 @@ import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-img-carusel',
-  templateUrl: './img-carusel.component.html'
+  templateUrl: './img-carusel.component.html',
+  styleUrls: ['./img-carusel.component.css']
 })
 export class ImgCaruselComponent implements OnInit {
 
